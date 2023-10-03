965532da870bbae4997964698755c40e111daa3a
2814109 <2814109k@gmail.com>
2023-10-04 02:09:49 +0900

    change db port

--- app/server.go
+++ app/server.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"app/graph"
+	// "github.com/uptrace/bunrouter"
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,6 +13,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 
 	"database/sql"
+	_ "github.com/lib/pq"
 
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
@@ -20,18 +24,31 @@ const defaultPort = "8081"
 
 func main() {
 
-	dsn := "postgres://postgres:@stock-postgres:5433/?sslmode=disable"
+	dsn := "postgres://postgres:postgres@stock-postgres:5433/postgres?sslmode=disable"
 	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
 
 	db := bun.NewDB(sqldb, pgdialect.New())
-	log.Printf("%v", db)
+
+	var v string
+	if err := db.NewSelect().ColumnExpr("version()").Scan(context.Background(), &v); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(v)
+	log.Printf("success %v", db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	// router := bunrouter.New()
 
+	// router.GET("/", func(w http.ResponseWriter, req bunrouter.Request) error {
+	// 	// req embeds *http.Request and has all the same fields and methods
+	// 	fmt.Println(req.Method, req.Route(), req.Params().Map())
+	// 	return w.playground.Handler("GraphQL playground", "/query")
+	// })
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
